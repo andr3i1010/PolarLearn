b5f36aadb1c9af2cc92c52537c9c31c4cecde354
Maaren <80040706+mariomaaren@users.noreply.github.com>
2025-03-14 08:01:31 +0100

    Blokjes weghalen bij geen recente vakken
    ik heb ook wat placeholders verwijderd, dit is een kleine verandering maar ziet er wel mooieruit

--- src/app/home/start/page.tsx
+++ src/app/home/start/page.tsx
@@ -127,101 +127,26 @@ export default async function Start() {
           <div>
             <div className="flex pt-5 pl-5 space-x-4 relative overflow-hidden w-screen">
               {recentSubjects.length === 0 && (
-                <>
-                  <p className="absolute top-[35px] w-full pl-9 text-neutral-400 font-bold">
-                    Je hebt nog geen vakken geoefend. Leer een lijst van een bepaalde vak, en de geoefende vak van de lijst komt hier.
-                  </p>
-                  <div className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"></div>
-
-                  <div className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"></div>
-                  <div className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"></div>
-                  <div className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"></div>
-                  <div className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"></div>
-                  <div className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"></div>
-                </>
+                <p className="absolute top-[35px] w-full pl-9 text-neutral-400 font-bold">
+                  Je hebt nog geen vakken geoefend. Leer een lijst van een bepaalde vak, en de geoefende vak van de lijst komt hier.
+                </p>
               )}
-              {recentSubjects.map((subject: string, index: number) => (
-                <div
-                  key={index}
-                  className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
-                >
-                  {
-                    (() => {
-                      return subjectEmojiMap[subject] ? subjectEmojiMap[subject] : "";
-                    })()
-                  }
-                </div>
-              ))}
-            </div>
-            <div className="h-3" />
-            <div className="flex items-center text-center">
-              <h1 className="text-4xl pl-5 pt-4 mb-2 font-extrabold">Recente Lijsten:</h1>
-              <div className="ml-auto mr-5">
-                <PlusBtn redir="/learn/createlist" />
-              </div>
-            </div>
-            <div className="h-4" />
-            <div className="space-y-4">
-              {recentLists.length == 0 && (
-                <>
-                  <div className="tile bg-neutral-800 text-neutral-400 text-xl font-bold py-2 px-4 mx-4 rounded-lg h-20 text-center place-items-center grid">
-                    Je hebt nog geen lijsten geoefend. Leer een lijst, en de geoefende lijst komt hier.
+              {recentSubjects.length > 0 && (
+                recentSubjects.map((subject: string, index: number) => (
+                  <div
+                    key={index}
+                    className="tile bg-neutral-800 text-white font-bold py-2 px-4 rounded-lg w-36 h-14 text-center place-items-center grid"
+                  >
+                    {subjectEmojiMap[subject] ? subjectEmojiMap[subject] : ""}
                   </div>
-                  <div className="tile bg-neutral-800 text-white font-bold py-2 px-4 mx-4 rounded-lg h-20 text-center place-items-center grid "></div>
-                </>
-              )}
-              {recentLists.length > 0 && (
-                <>
-                  {recentLists.map((list: any, index: number) => (
-                    <div >
-                      <Link href={`/learn/viewlist/${list.list_id}`} key={index} >
-                        <div className="tile bg-neutral-800 hover:bg-neutral-700 transition-colors text-white font-bold py-2 px-6 mx-4 rounded-lg h-20 flex items-center justify-between cursor-pointer">
-                          <div className="flex flex-col text-left">
-                            <div className="flex items-center">
-                              {list.subject && (
-                                <Image
-                                  src={
-                                    list.subject === "NL" ? nl_img :
-                                      list.subject === "DE" ? de_img :
-                                        list.subject === "FR" ? fr_img :
-                                          list.subject === "EN" ? eng_img :
-                                            list.subject === "WI" ? math_img :
-                                              list.subject === "NSK" ? nsk_img :
-                                                list.subject === "GS" ? gs_img :
-                                                  list.subject === "BI" ? bi_img : ''
-                                  }
-                                  alt={`${list.subject} icon`}
-                                  width={24}
-                                  height={24}
-                                  className="mr-2"
-                                />
-                              )}
-                              <span className="text-lg">{list.name}</span>
-                            </div>
-                          </div>
-
-                          {list.creator && (
-                            <div className="flex items-center text-sm text-neutral-400">
-                              <CreatorLink creator={list.creator} />
-                            </div>
-                          )}
-
-                          <div className="text-neutral-400 text-sm">
-                            {Array.isArray(list.data) && list.data.length === 1
-                              ? "1 woord"
-                              : `${Array.isArray(list.data) ? list.data.length : 0} woorden`}
-                          </div>
-                        </div>
-                      </Link>
-                    </div>
-                  ))}
-                </>
+                ))
               )}
             </div>
+
+            <div className="h-3" />
           </div>
         </div>
-      </div >
-      <div className="h-4" />
+      </div>
     </>
   );
 }
